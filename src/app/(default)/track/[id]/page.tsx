93c85488573800566954acf8e01a430759278be7
M4rshe1 <colin.heggli@gmail.com>
2026-05-29 19:55:17 +0200

    added missing HydrateClient components

--- src/app/(default)/track/[id]/page.tsx
+++ src/app/(default)/track/[id]/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { PageBreadcrumbs } from "@/components/page-breadcrumbs";
 import { getPreferredMetricsInput } from "@/lib/get-preferred-metrics-input";
 import { withAuth } from "@/lib/hoc-pages";
-import { api } from "@/trpc/server";
+import { api, HydrateClient } from "@/trpc/server";
 import ClientPage from "./_components/client-page";
 
 export async function generateMetadata(props: {
@@ -57,7 +57,9 @@ const Page = withAuth(async ({ params }: { params: { id: string } }) => {
           { label: track.name, href: `/track/${track.id}` },
         ]}
       />
-      <ClientPage id={numericId} />
+      <HydrateClient>
+        <ClientPage id={numericId} />
+      </HydrateClient>
     </div>
   );
 });
